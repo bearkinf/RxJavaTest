25f403c8a1eb637d7afbdc97e000ea7186be64a4
bearkinf-waem <bearkinf@waem.kr>
2023-08-23 15:37:48 +0900

    test code

--- app/src/main/java/com/example/rxjavatest/MainActivity.java
+++ app/src/main/java/com/example/rxjavatest/MainActivity.java
@@ -18,69 +18,6 @@ public class MainActivity extends AppCompatActivity {
     CompositeDisposable disposable = new CompositeDisposable();
 
 
-    void gogodan(int number) {
-
-        LogPrintUtil.i("number : " + number);
-//        Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9)
-//
-//                .map(integer -> {
-//                    return number * integer.intValue();
-//                })
-//
-//                .subscribe(integer -> {
-//
-//                            buffer.append("gogodan : " + integer + "\n");
-//                            binding.HelloWorld.setText(buffer.toString());
-//
-//                        }
-//                );
-
-        disposable.add(Observable.range(1, 9)
-                .subscribeOn(Schedulers.io())
-                .observeOn(Schedulers.io())
-                .map(integer -> {
-                    LogPrintUtil.w(".subscribeOn(Schedulers.io())  : " + integer);
-                    return integer;
-                })
-                .observeOn(Schedulers.io())
-                .map(integer -> {
-
-                    LogPrintUtil.v(".observeOn(Schedulers.io())  : " + integer);
-                    int gogo = number * integer.intValue();
-                    return buffer.append("gogodan : " + gogo + "\n").toString();
-                })
-
-
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(s -> {
-                    LogPrintUtil.e(" .observeOn(AndroidSchedulers.mainThread())  :\n" + s);
-
-                }, throwable -> {
-                    throwable.printStackTrace();
-                }, () -> {
-
-                    LogPrintUtil.e(" .observeOn(AndroidSchedulers.mainThread())  :  onComplete");
-                    buffer.append("onComplete");
-                    binding.HelloWorld.setText(buffer.toString());
-                })
-        );
-
-//                .subscribe(integer -> {
-//
-////                            buffer.append("gogodan : " + integer + "\n");
-////                            binding.HelloWorld.setText(buffer.toString());
-//
-//                        },
-//                        throwable -> {
-//                        },
-//                        () -> {
-//                            buffer.append("onComplete");
-//                            binding.HelloWorld.setText(buffer.toString());
-//                        }
-//                );
-
-    }
-
     private StringBuffer buffer = new StringBuffer();
 
 
@@ -89,18 +26,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         LogPrintUtil.setDebug(true);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.startBtn.setOnClickListener(v -> {
 
-            if (binding.editText.getText().length() == 0 || binding.editText.getText().equals("")) {
-                return;
-            }
-            buffer = new StringBuffer();
-            gogodan(Integer.parseInt(binding.editText.getText().toString()));
+        binding.startBtn.setOnClickListener(v -> {
+            v.setEnabled(false);
+
+            v.postDelayed(() -> {
+                v.setEnabled(true);
+                LogPrintUtil.e("click enabled");
+
+
+            }, 1000);
+
+
         });
+
+
+//        binding.startBtn.setOnClickListener(v -> {
+//
+//            if (binding.editText.getText().length() == 0 || binding.editText.getText().equals("")) {
+//                return;
+//            }
+//            buffer = new StringBuffer();
+//            gogodan(Integer.parseInt(binding.editText.getText().toString()));
+//        });
 
 
 //        // 연속적인 flatmap 사용으로 데이터를 계속 처리하려면?????
@@ -171,4 +122,69 @@ public class MainActivity extends AppCompatActivity {
         Log.d("bear", "destory:");
         disposable.dispose();
     }
+
+
+    void gogodan(int number) {
+
+        LogPrintUtil.i("number : " + number);
+//        Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9)
+//
+//                .map(integer -> {
+//                    return number * integer.intValue();
+//                })
+//
+//                .subscribe(integer -> {
+//
+//                            buffer.append("gogodan : " + integer + "\n");
+//                            binding.HelloWorld.setText(buffer.toString());
+//
+//                        }
+//                );
+
+        disposable.add(Observable.range(1, 9)
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.io())
+                .map(integer -> {
+                    LogPrintUtil.w(".subscribeOn(Schedulers.io())  : " + integer);
+                    return integer;
+                })
+                .observeOn(Schedulers.io())
+                .map(integer -> {
+
+                    LogPrintUtil.v(".observeOn(Schedulers.io())  : " + integer);
+                    int gogo = number * integer.intValue();
+                    return buffer.append("gogodan : " + gogo + "\n").toString();
+                })
+
+
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(s -> {
+                    LogPrintUtil.e(" .observeOn(AndroidSchedulers.mainThread())  :\n" + s);
+
+                }, throwable -> {
+                    throwable.printStackTrace();
+                }, () -> {
+
+                    LogPrintUtil.e(" .observeOn(AndroidSchedulers.mainThread())  :  onComplete");
+                    buffer.append("onComplete");
+                    binding.HelloWorld.setText(buffer.toString());
+                })
+        );
+
+//                .subscribe(integer -> {
+//
+////                            buffer.append("gogodan : " + integer + "\n");
+////                            binding.HelloWorld.setText(buffer.toString());
+//
+//                        },
+//                        throwable -> {
+//                        },
+//                        () -> {
+//                            buffer.append("onComplete");
+//                            binding.HelloWorld.setText(buffer.toString());
+//                        }
+//                );
+
+    }
+
 }
